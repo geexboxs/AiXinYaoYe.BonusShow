9d49413e05102ed59145a1c64d7eb9729353c6e4
mrkkk <mrkkk1994@outlook.com>
2018-03-19 12:34:12 +0800

    sdfsdf

--- AiXinYaoYe/WeChat/WXConfig.cs
+++ AiXinYaoYe/WeChat/WXConfig.cs
@@ -7,7 +7,7 @@ namespace AiXinYaoYe.Config
 {
     public class WXConfig
     {
-        public const string APPID = "123";
+        public const string APPID = "wx3c0a54573d979889";
         public const string APPSECRET = "321";
     }
 }
